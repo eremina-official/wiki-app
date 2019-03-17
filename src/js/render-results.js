354cfaf58ab70e3f02dbaf6ddcf3e227c7f85f6e
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-17 20:00:41 +0100

    edit renderSearchRestuls() function

--- src/js/render-results.js
+++ src/js/render-results.js
@@ -5,17 +5,18 @@ const searchResults = document.querySelector('.search-results');
 
 /* render search results */
 function renderSearchResults(searchData) {
-  if (searchData) {
-    //snippet.innerHTML = searchData.query.search[2].snippet;
-    const resultArray = searchData.query.search;
+  const resultArray = searchData.query.search;
+  if (resultArray.length) {
     resultArray.forEach(element => {
       const searchResultsItem = document.createElement('div');
+      searchResultsItem.setAttribute('class', 'search-results__item');
 
       const title = document.createElement('p');
       title.textContent = element.title;
 
       const snippet = document.createElement('p');
-      snippet.innerHTML = element.snippet;
+      snippet.setAttribute('class', 'snippet');
+      snippet.innerHTML = `${element.snippet}...`;
 
       searchResultsItem.appendChild(title);
       searchResultsItem.appendChild(snippet);
