8bf1cd51cf892d6c287cc5c2b8cb67b5508d2a7c
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-17 18:42:45 +0100

    update renderSearchResults() function

--- src/js/render-results.js
+++ src/js/render-results.js
@@ -1,12 +1,28 @@
-import { searchData } from './send-request.js';
-
 //cache DOM
-const snippet = document.querySelector('.snippet');
+const searchResults = document.querySelector('.search-results');
 
 //function declarations
-function renderSearchResults() {
+
+/* render search results */
+function renderSearchResults(searchData) {
   if (searchData) {
-    snippet.innerHTML = searchData.query.search[2].snippet;
+    //snippet.innerHTML = searchData.query.search[2].snippet;
+    const resultArray = searchData.query.search;
+    resultArray.forEach(element => {
+      const searchResultsItem = document.createElement('div');
+
+      const title = document.createElement('p');
+      title.textContent = element.title;
+
+      const snippet = document.createElement('p');
+      snippet.innerHTML = element.snippet;
+
+      searchResultsItem.appendChild(title);
+      searchResultsItem.appendChild(snippet);
+      searchResults.appendChild(searchResultsItem);
+    })
+  } else {
+    searchResults.textContent = 'No results found.';
   }
 }
 
