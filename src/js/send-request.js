894f1383c744cf2d3f78be13ac6062e81cb7515e
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-17 21:45:22 +0100

    add comments

--- src/js/send-request.js
+++ src/js/send-request.js
@@ -9,11 +9,13 @@ form.addEventListener('submit', makeSearch);
 
 //function declarations
 function makeSearch(event) {
+  /* prevent default form submission and add custom form handling */
   event.preventDefault();
   const url = composeUrl();
   makeRequest(url);
 }
 
+/* compose url to query API using the search keywords */
 function composeUrl() {
   const keywords = input.value;
   /* origin parameter must be set to wildcard to allow for unauthenticated CORS requests */
@@ -21,10 +23,11 @@ function composeUrl() {
   return url;
 }
 
+/* query API */
 function makeRequest(url) {
   fetch(url)
     .then(response => response.json())
-    /* .json() method returns a promise, therefore it's neede to chain one more .then() method */
+    /* .json() method returns a promise, therefore it's needed to chain one more .then() method */
     .then((searchData) => {
       renderSearchResults(searchData);
       console.log(searchData);
