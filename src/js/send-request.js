ebda408ceb0ba53bcaab81ed323f41c5cc521743
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-18 14:12:00 +0100

    add request to extracts to url, compose url from params object

--- src/js/send-request.js
+++ src/js/send-request.js
@@ -3,6 +3,17 @@ import { renderSearchResults } from './render-results.js';
 //cache DOM
 const form = document.querySelector('.js-form');
 const input = document.querySelector('.js-input-keyword');
+const apiUrl = 'https://en.wikipedia.org/w/api.php';
+const params = {
+  action: 'action=query',
+  generator: 'generator=search',
+  prop: 'prop=info|extracts',
+  inprop: 'inprop=url',
+  exprop: 'exintro=&explaintext=&exsentences=1',
+  format: 'format=json',
+  /* origin parameter must be set to wildcard to allow for unauthenticated CORS requests */
+  origin: 'origin=*'
+};
 
 //bind events
 form.addEventListener('submit', makeSearch);
@@ -17,13 +28,12 @@ function makeSearch(event) {
 
 /* compose url to query API using the search keywords */
 function composeUrl() {
-  const keywords = input.value;
-  /* origin parameter must be set to wildcard to allow for unauthenticated CORS requests */
-  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${keywords}&utf8=&format=json&origin=*`;
+  const keywords = input.value;  
+  const url = `${apiUrl}?${params.action}&${params.generator}&gsrsearch=${keywords}&${params.prop}&${params.inprop}&${params.exprop}&${params.format}&${params.origin}`;
   return url;
 }
 
-/* query API */
+/* call the API */
 function makeRequest(url) {
   fetch(url)
     .then(response => response.json())
@@ -31,6 +41,8 @@ function makeRequest(url) {
     .then((searchData) => {
       renderSearchResults(searchData);
       console.log(searchData);
+      //console.log(typeof searchData.query.pages);
+      //console.log(Object.keys(searchData.query.pages))
     })
     .catch(error => console.log(error));
 /*   fetch('https://en.wikipedia.org/w/api.php?action=query&pageids=33454&prop=info&inprop=url&format=json&origin=*')
