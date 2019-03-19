711f4c3ed1628b9742e4d46225bd557aa2ea3784
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-19 20:36:46 +0100

    add function declarations for handling langlinks request

--- src/js/send-request.js
+++ src/js/send-request.js
@@ -1,4 +1,7 @@
+/* this module handles requests to the wiki API */
+
 import { renderSearchResults } from './render-results.js';
+import { renderLanguages } from './show-languages.js';
 
 //cache DOM
 const form = document.querySelector('.js-form');
@@ -7,7 +10,8 @@ const apiUrl = 'https://en.wikipedia.org/w/api.php';
 const params = {
   action: 'action=query',
   generator: 'generator=search',
-  prop: 'prop=info|extracts|langlinks',
+  prop: 'prop=info|extracts',
+  lprop: 'prop=langlinks',
   inprop: 'inprop=url',
   exprop: 'exintro=&explaintext=&exsentences=1',
   llprop: 'llprop=url|autonym',
@@ -17,6 +21,7 @@ const params = {
   origin: 'origin=*'
 };
 let currentSearchData;
+let currentLangData;
 
 //bind events
 form.addEventListener('submit', makeSearch);
@@ -26,29 +31,40 @@ function makeSearch(event) {
   /* prevent default form submission and add custom form handling */
   event.preventDefault();
   const url = composeUrl();
-  makeRequest(url);
+  makeRequest(url, currentSearchData, renderSearchResults);
 }
 
 /* compose url to query API using the search keywords */
 function composeUrl() {
   const keywords = input.value;  
-  const url = `${apiUrl}?${params.action}&${params.generator}&gsrsearch=${keywords}&${params.prop}&${params.inprop}&${params.exprop}&${params.llprop}&${params.lllimit}&${params.format}&${params.origin}`;
+  const url = `${apiUrl}?${params.action}&${params.generator}&gsrsearch=${keywords}&${params.prop}&${params.inprop}&${params.exprop}&${params.format}&${params.origin}`;
+  return url;
+}
+
+/* request languages. Languages are requested in a separate query because the wiki API returns
+not more that 500 langlinks per one request, and for ten wiki pages there is usually more languages linked. */
+function requestLanguages(pageId) {
+  const langUrl = composeLangUrl(pageId);
+  makeRequest(langUrl, currentLangData, renderLanguages);
+}
+
+function composeLangUrl(pageId) {
+  const url = `${apiUrl}?${params.action}&pageids=${pageId}&${params.lprop}&${params.llprop}&${params.lllimit}&${params.format}&${params.origin}`;
   return url;
 }
 
 /* call the API */
-function makeRequest(url) {
+function makeRequest(url, dataStorage, callback) {
   fetch(url)
     .then(response => response.json())
     /* .json() method returns a promise, therefore it's needed to chain one more .then() method */
     .then((searchData) => {
-      renderSearchResults(searchData);
-      currentSearchData = searchData;
-      console.log(searchData);
-      //console.log(typeof searchData.query.pages);
-      //console.log(Object.keys(searchData.query.pages))
+      callback(searchData);
+      dataStorage = searchData;
+      console.log(currentSearchData);
+      console.log(currentLangData);
     })
     .catch(error => console.log(error));
 }
 
-export { currentSearchData };
+export { requestLanguages };
