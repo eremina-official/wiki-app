f0f6aa017411129c539b10be73dfab90b23620d5
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-20 13:57:15 +0100

    refactor renderLanguages() function

--- src/js/show-languages.js
+++ src/js/show-languages.js
@@ -2,6 +2,12 @@
 
 import { requestLanguages } from './send-request.js';
 
+//declare variables
+/* page and pageId declared in the module scope so that both showLanguages() 
+and renderLanguages() had access to them */
+let page;
+let pageId;
+
 //bind events
 document.addEventListener('click', showLanguages);
 
@@ -15,8 +21,8 @@ function showLanguages(event) {
   if (event.target.nextElementSibling) {
     event.target.nextElementSibling.classList.toggle('is-not-active');
   } else {
-    const page = event.target.parentElement;
-    const pageId = page.getAttribute('id');
+    page = event.target.parentElement;
+    pageId = page.getAttribute('id');
     requestLanguages(pageId);
   }
 }
@@ -24,31 +30,34 @@ function showLanguages(event) {
 /* render languages when the Show/Hide languages button is clicked for the first time */
 function renderLanguages(searchData) {
 console.log(searchData);
-/*   const langlinks = currentSearchData.query.pages[pageId]['langlinks'];
+  const langlinks = searchData.query.pages[pageId]['langlinks'];
   const langContainer = document.createElement('div');
   langContainer.setAttribute('class', 'lang-container js-lang-container');
 
-  langlinks.forEach(langlink => {
-    const langNativeName = document.createElement('p');
-    langNativeName.textContent = langlink.autonym;
+  if (langlinks) {
+    langlinks.forEach(langlink => {
+      const langNativeName = document.createElement('p');
+      langNativeName.textContent = langlink.autonym;
 
-    const langTitle = document.createElement('a');
-    langTitle.setAttribute('class', 'lang-title');
-    langTitle.setAttribute('lang', langlink.lang);
-    langTitle.setAttribute('href', langlink.url);
+      const langTitle = document.createElement('a');
+      langTitle.setAttribute('class', 'lang-title');
+      langTitle.setAttribute('lang', langlink.lang);
+      langTitle.setAttribute('href', langlink.url);
 
-     bracket notation is used to embed a symbol 
-    langTitle.textContent = langlink['*'];
+      /* bracket notation is used to embed a symbol */
+      langTitle.textContent = langlink['*'];
 
-    const langContainerItem = document.createElement('div');
-    langContainerItem.setAttribute('class', 'lang-container__item');
+      const langContainerItem = document.createElement('div');
+      langContainerItem.setAttribute('class', 'lang-container__item');
 
-    langContainerItem.appendChild(langNativeName);
-    langContainerItem.appendChild(langTitle);
-    langContainer.appendChild(langContainerItem);
-  });
-
-  page.appendChild(langContainer); */
+      langContainerItem.appendChild(langNativeName);
+      langContainerItem.appendChild(langTitle);
+      langContainer.appendChild(langContainerItem);
+    });
+  } else {
+    langContainer.textContent = 'There are no interlanguage links for this page.'
+  }
+  page.appendChild(langContainer); 
 }
 
 export { renderLanguages };
