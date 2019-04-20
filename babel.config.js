35438d29c6187eb192b71d6054edd3fca34a22e5
Marina Eremina <eremina.jobmail@gmail.com>
2019-04-20 17:24:44 +0200

    add babel config file

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+/* wiki-app */
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
