e9032ddec5b84172ca60453377cc30a5e3b9ea0d
Marina Eremina <eremina.jobmail@gmail.com>
2019-04-23 21:47:40 +0200

    edit comments

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 /* wiki-app */
 
+/* 
+  babel.config.js file is added so that Jest could transform 
+  and run js files which use es6 import/export statements, 
+  since Jest does not support es6 modules (as for 04.2019). 
+  By default, if Jest sees a Babel config, it will use that 
+  to transform js files, ignoring "node_modules".
+*/
 module.exports = {
   presets: [ '@babel/preset-env' ]
 };
