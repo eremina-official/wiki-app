e0ba80430b2d681345eff35503125c2a383c0512
Marina Eremina <eremina.jobmail@gmail.com>
2019-10-23 13:32:59 +0200

    add wiki app tests

--- cypress/integration/wiki-app.spec.js
+++ cypress/integration/wiki-app.spec.js
@@ -1,22 +1,73 @@
 describe('wiki app', () => {
-  it('displays results for web', () => {
-    
+  beforeEach(() => {
     const searchKeyword = 'https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=Web&gsrlimit=20&prop=info|extracts&inprop=url&exintro=&explaintext=&exsentences=1&format=json&origin=*';
 
-    /* 
-      Writing of json reponce to the fixtures file for a specific url.
-      cy.request(searchUrl).then(responce => {
-      cy.writeFile("cypress/fixtures/web.json", responce.body)
-      }) 
-    */
+    const searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&titles=World Wide Web&prop=info|extracts&inprop=url&exintro=&explaintext=&exsentences=1&format=json&origin=*';
 
     cy.server()
     cy.route('GET', searchKeyword, 'fixture:web')
     cy.route('GET', searchUrl, 'fixture:url')
     cy.visit('/')
+  })
 
+  it('displays results for keywords and specific urls', () => {
     cy.get('.keyword-input')
       .type('Web')
-      .type('{enter}')  
+      .type('{enter}')
+    cy.url()
+      .should('eq', 'https://eremina-official.github.io/wiki-app/?q=Web')
+    cy.get('.search-results')
+      .children().should('have.length', 10)
+    cy.get('.search-results__item:first-child')
+      .should('have.id', '33120')
+
+    cy.get('.keyword-input').clear()
+      .type('https://en.wikipedia.org/wiki/World_Wide_Web')
+      .type('{enter}')
+    cy.url()
+      .should('eq', 'https://eremina-official.github.io/wiki-app/?q=https://en.wikipedia.org/wiki/World_Wide_Web')
+    cy.get('.search-results')
+      .children().should('have.length', 1)
+    cy.get('.search-results__item:first-child')
+      .should('have.id', '33139')
+
+    cy.go('back')
+    cy.url()
+      .should('eq', 'https://eremina-official.github.io/wiki-app/?q=Web')
+    cy.get('.search-results')
+      .children().should('have.length', 10)
+    cy.get('.search-results__item:first-child')
+      .should('have.id', '33120')
+
+    cy.go('forward')
+    cy.url()
+      .should('eq', 'https://eremina-official.github.io/wiki-app/?q=https://en.wikipedia.org/wiki/World_Wide_Web')
+    cy.get('.search-results')
+      .children().should('have.length', 1)
+    cy.get('.search-results__item:first-child')
+      .should('have.id', '33139')
+  })
+
+  it.only('shows titles in available languages', () => {
+    cy.get('.keyword-input')
+      .type('Web')
+      .type('{enter}')
+
+    cy.get('.search-results__item:first-child > .lang-container')
+      .should('not.exist')
+    cy.get('.search-results__item:first-child > .button-language').click()
+    cy.get('.search-results__item:first-child > .lang-container')
+      .should('exist')
+      .and('not.have.class', 'is-not-active')
+    cy.get('.search-results__item:first-child > .lang-container')
+      .children()
+      .should('have.length', 38)
+    cy.get('.search-results__item:first-child > .button-language').click()
+    cy.get('.search-results__item:first-child > .lang-container')
+      .should('exist')
+      .and('have.class', 'is-not-active')
   })
 })
+
+
+
