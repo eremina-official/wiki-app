b6d154bf1cac20c157ec5bcd872b1eab90271abe
Marina Eremina <eremina.jobmail@gmail.com>
2019-11-18 13:39:42 +0100

    edit tests

--- cypress/integration/write.spec.js
+++ cypress/integration/write.spec.js
@@ -4,15 +4,23 @@ describe('create fixtures', () => {
 
   const searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&titles=World%20Wide%20Web&prop=info|extracts&inprop=url&exintro=&explaintext=&exsentences=1&format=json&origin=*';
 
-  it('writes api responce for searchKeyword to the fixtures file', () => {
+  const languageUrl = 'https://en.wikipedia.org/w/api.php?action=query&pageids=33120&prop=langlinks&llprop=url|autonym&lllimit=300&format=json&origin=*';
+
+  it('writes api responce for searchKeyword to the fixture file', () => {
     cy.request(searchKeyword).then(responce => {
-      cy.writeFile("cypress/fixtures/web.json", responce.body)
+      cy.writeFile('cypress/fixtures/web.json', responce.body)
     }) 
   })
 
-  it.only('writes api responce for searchUrl to the fixtures file', () => {
+  it('writes api responce for searchUrl to the fixture file', () => {
     cy.request(searchUrl).then(responce => {
-      cy.writeFile("cypress/fixtures/searchUrl.json", responce.body)
+      cy.writeFile('cypress/fixtures/searchUrl.json', responce.body)
+    })
+  })
+
+  it.only('writes api responce for languageUrl to the fixture file', () => {
+    cy.request(languageUrl).then(responce => {
+      cy.writeFile('cypress/fixtures/languageUrl.json', responce.body)
     })
   })
 })
